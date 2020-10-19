7536675933aefa97c79be100fd5f95ef4b2c15f0
RogueNPC <jordanbtansay@gmail.com>
2020-10-19 10:56:54 -0700

    Simple skateboard script

--- scripts.js
+++ scripts.js
@@ -1,5 +1,10 @@
 const h2 = document.querySelector('h2')
 
 function replace_text() {
-    h2.innerText = "Take a jog outside"
+    const activities = ["Take a short jog outside", "Do a couple pushups", "Do a couple jumping jacks", "Take a few deep breaths",
+                        "Yell into a pillow", "Read a book", "Eat a snack", "Take a nap"]
+
+    const random = Math.floor(Math.random() * activities.length)
+
+    h2.innerText = activities[random]
 }
