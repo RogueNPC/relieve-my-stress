aa9158de4e0345481df743300b0fe393b9aac362
RogueNPC <jordanbtansay@gmail.com>
2020-10-19 11:57:49 -0700

    Added radio button function

--- scripts.js
+++ scripts.js
@@ -1,10 +1,46 @@
 const h2 = document.querySelector('h2')
 
 function replace_text() {
-    const activities = ["Take a short jog outside", "Do a couple pushups", "Do a couple jumping jacks", "Take a few deep breaths",
-                        "Yell into a pillow", "Read a book", "Eat a snack", "Take a nap"]
+    const allActivities = ["Take a short jog outside", "Do a couple pushups", "Do a couple jumping jacks", 
+                            "Take a few deep breaths", "Yell into a pillow", "Read a book", "Eat a snack", 
+                            "Compliment yourself", "Play a singleplayer game", "Start a friendly conversation with someone",
+                            "Go to the gym", "Hug a stuffed animal/pillow/family member",
+                            "Solve a sudoku/crossword/jigsaw puzzle", "Listen to some music", "Do some stretches",
+                            "Meditate for a few minutes", "Remember a time when you were relaxed and enjoying yourself"]
 
-    const random = Math.floor(Math.random() * activities.length)
+    const exerciseActivities = ["Take a short jog outside", "Do a couple pushups", "Do a couple jumping jacks",
+                                "Go to the gym", "Do some stretches"]
 
-    h2.innerText = activities[random]
+    const mentalActivities = ["Take a few deep breaths", "Yell into a pillow", "Compliment yourself",
+                                "Meditate for a few minutes", "Remember a time when you were relaxed and enjoying yourself"]
+                                
+    const comfortActivities = ["Read a book", "Eat a snack", "Play a single player game", 
+                                "Start a friendly conversation with someone", "Hug a stuffed animal/pillow/family member",
+                                "Solve a sudoku/crossword/jigsaw puzzle", "Listen to some music"]
+
+    const listChoice = Array.from(document.querySelectorAll('input[type=radio]'))
+    let selectedList;
+    listChoice.forEach((list) => {
+        if (list.checked) {
+            selectedList = list.value
+        }
+    })
+    //console.log(selectedList)
+
+    if (selectedList === "exercise"){
+        const random = Math.floor(Math.random() * exerciseActivities.length)
+        h2.innerText = exerciseActivities[random]
+    }
+    else if (selectedList === "mental"){
+        const random = Math.floor(Math.random() * mentalActivities.length)
+        h2.innerText = mentalActivities[random]
+    }
+    else if (selectedList === "comfort"){
+        const random = Math.floor(Math.random() * comfortActivities.length)
+        h2.innerText = comfortActivities[random]
+    }
+    else {
+        const random = Math.floor(Math.random() * allActivities.length)
+        h2.innerText = allActivities[random]
+    }
 }
